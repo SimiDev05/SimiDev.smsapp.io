6e92d0120605937a11201bbe8d7cf4e545999e8f
SIMI <demogit222@gmail.com>
2017-10-26 17:32:31 +0700

    C3

--- smsapp/smsapp/settings.py
+++ smsapp/smsapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dashboard.apps.dashboardconfig',
+    #'dashboard.apps.dashboardconfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
