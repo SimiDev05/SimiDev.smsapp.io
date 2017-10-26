d36a1e7ff4164345c377b92dd6da5e682e105946
SIMI <demogit222@gmail.com>
2017-10-26 17:03:13 +0700

    2nd commit

--- smsapp/dashboard/views.py
+++ smsapp/dashboard/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse("<hi>This is the stident's homepage")
+	return HttpResponse("<hi>This is the stident's homepage<h1/>")
