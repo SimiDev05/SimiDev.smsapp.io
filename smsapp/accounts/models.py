e018ef140dfb900191e35c54d539eed0df92d6e7
SIMI <demogit222@gmail.com>
2017-11-09 14:48:16 +0700

    Create Model

--- smsapp/accounts/models.py
+++ smsapp/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	#description = models.CharField(max_lenght=250, default='')
+	age = models.IntegerField(default=0)
+	#Gender = models.CharField
+	Grade = models.IntegerField(default=0)
+	#Address = models.CharField(max_lenght=250)
+	#Email = models.charfield(max_lenght=250)
+	Phonenumber = models.IntegerField(default=0) 
+
