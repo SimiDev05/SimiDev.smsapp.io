09442e3bcc51013b58b67fe4ceafc63f0c835f46
SIMI <demogit222@gmail.com>
2017-12-15 18:07:42 +0700

    namespace

--- smsapp/smsapp/urls.py
+++ smsapp/smsapp/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
 
     url(r'^$', views.login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
-    url(r'^dashboard/', include('dashboard.urls')),
-    url(r'^accounts/', include('accounts.urls')),
+    url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
+    url(r'^accounts/', include('accounts.urls', namespace= 'accounts')),
    
 
     ]
